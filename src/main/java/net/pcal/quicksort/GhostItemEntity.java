610f8358d1eace7764bca3a1d7e824ec68da40bb
Patrick Calahan <github@pcal.net>
2022-07-19 15:24:52 -0700

    meh

--- src/main/java/net/pcal/quicksort/GhostItemEntity.java
+++ src/main/java/net/pcal/quicksort/GhostItemEntity.java
@@ -5,17 +5,19 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+/**
+ * ItemEntity that tries to disable collision checking.  The only one that really seems to work here
+ * is onPlayerCollision (but that's the most important one).
+ */
 public class GhostItemEntity extends ItemEntity {
 
     public GhostItemEntity(World world, double d, double e, double f, ItemStack stack) {
         super(world, d, e, f, stack);
     }
 
-
     @Override
     protected void checkBlockCollision() {
     }
-
 
     public void onPlayerCollision(PlayerEntity player) {
         // never collide with a player
