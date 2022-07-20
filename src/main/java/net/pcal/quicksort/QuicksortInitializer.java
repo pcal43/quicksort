ab1c5be56a5288d90ee2ff7672f4bd1aa24bb9bc
Patrick Calahan <github@pcal.net>
2022-07-19 21:22:02 -0700

    squash

--- src/main/java/net/pcal/quicksort/QuicksortInitializer.java
+++ src/main/java/net/pcal/quicksort/QuicksortInitializer.java
@@ -74,7 +74,7 @@ public class QuicksortInitializer implements ModInitializer {
             logger.info(LOG_PREFIX + "LogLevel set to " + config.logLevel());
         }
         QuicksortService.getInstance().init(config, logger);
-        ServerTickEvents.END_WORLD_TICK.register(QuicksortService.getInstance());
+        ServerTickEvents.START_WORLD_TICK.register(QuicksortService.getInstance());
         logger.info(LOG_PREFIX + "Initialized");
     }
 }
