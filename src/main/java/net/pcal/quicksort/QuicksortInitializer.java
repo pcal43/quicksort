39fd9da4c12073bd2196b846bb04d0c2cbd46c58
Patrick Calahan <github@pcal.net>
2023-03-19 22:48:25 -0700

    checkpoint

--- src/main/java/net/pcal/quicksort/QuicksortInitializer.java
+++ src/main/java/net/pcal/quicksort/QuicksortInitializer.java
@@ -7,11 +7,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
