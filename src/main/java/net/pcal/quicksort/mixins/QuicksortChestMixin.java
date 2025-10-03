224ee0489355c17e28d894e87f89b848f4b6d973
Patrick Calahan <github@pcal.net>
2025-10-02 18:57:44 -0700

    checkpoint

--- src/main/java/net/pcal/quicksort/mixins/QuicksortChestMixin.java
+++ src/main/java/net/pcal/quicksort/mixins/QuicksortChestMixin.java
@@ -1,5 +1,6 @@
 package net.pcal.quicksort.mixins;
 
+import net.minecraft.world.entity.ContainerUser;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.pcal.quicksort.QuicksortService;
@@ -12,13 +13,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class QuicksortChestMixin {
 
     @Inject(method = "stopOpen", at = @At("TAIL"))
-    public void onClose(Player player, CallbackInfo ci) {
+    public void stopOpen(ContainerUser user, CallbackInfo ci) {
         ChestBlockEntity e = (ChestBlockEntity) (Object) this;
         QuicksortService.getInstance().onChestClosed(e);
     }
 
     @Inject(method = "startOpen", at = @At("TAIL"))
-    public void onOpen(Player player, CallbackInfo ci) {
+    public void startOpen(ContainerUser user, CallbackInfo ci) {
         ChestBlockEntity e = (ChestBlockEntity) (Object) this;
         QuicksortService.getInstance().onChestOpened(e);
     }
